d65fb7098df939c83b1ddc029b8a62ab23f00cfc
keepConcentration <phm543@gmail.com>
2024-11-07 13:47:54 +0900

    [Bronze IV] Title: 한글 2, Time: 220 ms, Memory: 17640 KB -BaekjoonHub

---
+++ 백준/Bronze/11283. 한글 2/한글 2.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        String a = s.next();
+        char b = a.charAt(0);
+        System.out.println((int)b - 44031);
+    }
+}
