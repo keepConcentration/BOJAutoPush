5b5c561db3352f1c4910d86ccdc7af93c3480c93
keepConcentration <phm543@gmail.com>
2024-11-07 18:57:39 +0900

    [Unrated] Title: 더하기, Time: 200 ms, Memory: 17576 KB -BaekjoonHub

---
+++ 백준/Unrated/14918. 더하기/더하기.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+interface Main {
+    static void main(String[]z){
+Scanner sc = new Scanner(System.in);
+int a = sc.nextInt();
+int b = sc.nextInt();
+System.out.println((a+b));}}
