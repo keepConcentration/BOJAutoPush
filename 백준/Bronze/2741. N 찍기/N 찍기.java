209fa8ee97b53ed0a6464b2fb800703d4b529631
keepConcentration <phm543@gmail.com>
2024-11-07 13:09:06 +0900

    [Bronze V] Title: N 찍기, Time: 712 ms, Memory: 34852 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int loop = new Scanner(System.in).nextInt();
+        for (int i = 1; i <= loop; ++i) {
+            System.out.println(i);
+        }
+    }
+}
