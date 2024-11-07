3ba24ea23f2b27cf43bba75326ac896b7beb5323
keepConcentration <phm543@gmail.com>
2024-11-07 19:01:28 +0900

    [Silver III] Title: N과 M (4), Time: 112 ms, Memory: 18032 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+
+public class Main {
+    static int[] arr;
+    static int m;
+    static int n;
+    static StringBuilder sb;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        sb = new StringBuilder();
+        String[] param = br.readLine().split(" ");
+        br.close();
+        n = Integer.parseInt(param[0]);
+        m = Integer.parseInt(param[1]);
+        arr = new int[m];
+        
+        dfs(0, 0);
+        
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+    
+    public static void dfs(int depth, int a) {
+        if (depth == m) {
+            for (int val : arr) {
+                sb.append(String.valueOf(val));
+                sb.append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+        
+        for (int i = a; i < n; ++i) {
+            arr[depth] = i + 1;
+            dfs(depth + 1, i);
+        }
+    }
+}
