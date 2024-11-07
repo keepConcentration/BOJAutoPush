15667204f3564a1fb7ac284f7653bb906000d5d9
keepConcentration <phm543@gmail.com>
2024-11-07 13:44:52 +0900

    [Bronze III] Title: 별 찍기 - 16, Time: 72 ms, Memory: 11692 KB -BaekjoonHub

---
+++ 백준/Bronze/10991. 별 찍기 － 16/별 찍기 － 16.java
@@ -0,0 +1,32 @@
+import java.io.*;
+
+class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= N; ++i) {
+            int blank = N - i;
+            for (int j = 0; j < blank; ++j) {
+                sb.append(" ");
+            }
+
+            for (int j = 0; j < i * 2; ++j) {
+                if (j % 2 == 0) {
+                    sb.append("*");
+                } else {
+                    sb.append(" ");
+                }
+            }
+            sb.append("\n");
+        }
+
+        br.close();
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+}
