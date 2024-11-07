5f6d404829eb45695ab3b28b8fe126e5dc2295dd
keepConcentration <phm543@gmail.com>
2024-11-07 13:33:39 +0900

    [Silver III] Title: 돌 게임 5, Time: 80 ms, Memory: 11552 KB -BaekjoonHub

---
+++ 백준/Silver/9659. 돌 게임 5/돌 게임 5.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        long N = Long.parseLong(br.readLine());
+        br.close();
+        if (N % 2 == 0) {
+            bw.write("CY");
+        } else {
+            bw.write("SK");
+        }
+        bw.flush();
+        bw.close();
+    }
+}
