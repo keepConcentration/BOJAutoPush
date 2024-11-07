5a39a431ac9d1468768420a279b752bbd3e36d3d
keepConcentration <phm543@gmail.com>
2024-11-07 11:48:53 +0900

    [Unrated] Title: Mystery, Time: 76 ms, Memory: 11432 KB -BaekjoonHub

---
+++ 백준/Unrated/10212. Mystery/Mystery.java
@@ -0,0 +1,9 @@
+interface Main {
+    static void main(String[] args) {
+        if ((int)(Math.random() * 2.0) % 2 < 1) {
+            System.out.println("Yonsei");
+        } else {
+            System.out.println("Korea");
+        }
+    }
+}
