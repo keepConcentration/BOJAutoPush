d0a1e88e62e9b99b3a78b6a3ec914d2998dba428
keepConcentration <phm543@gmail.com>
2024-11-07 13:09:17 +0900

    [Bronze IV] Title: 기찍 N, Time: 664 ms, Memory: 35376 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        for (int i = new Scanner(System.in).nextInt(); i > 0; --i) {
+            System.out.println(i);
+        }
+    }
+}
