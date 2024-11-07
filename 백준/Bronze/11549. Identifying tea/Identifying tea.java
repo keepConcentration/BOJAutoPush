ac19a9345397aeee99fbdd019a5dd48462fc4a5e
keepConcentration <phm543@gmail.com>
2024-11-07 13:49:15 +0900

    [Bronze IV] Title: Identifying tea, Time: 204 ms, Memory: 17628 KB -BaekjoonHub

---
+++ 백준/Bronze/11549. Identifying tea/Identifying tea.java
@@ -0,0 +1,1 @@
+import java.util.*;interface Main{static void main(String[]z){Scanner s=new Scanner(System.in);int r=0,a=s.nextInt(),b=s.nextInt(),c=s.nextInt(),d=s.nextInt(),e=s.nextInt(),f=s.nextInt();r+=a==b?1:0;r+=a==c?1:0;r+=a==d?1:0;r+=a==e?1:0;r+=a==f?1:0;System.out.print(r);}}
