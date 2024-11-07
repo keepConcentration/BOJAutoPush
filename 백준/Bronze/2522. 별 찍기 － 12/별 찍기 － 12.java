12d64cf056bba4d96884f355608b5d056c6ffc3c
keepConcentration <phm543@gmail.com>
2024-11-07 13:02:53 +0900

    [Bronze III] Title: 별 찍기 - 12, Time: 540 ms, Memory: 24152 KB -BaekjoonHub

---
+++ 백준/Bronze/2522. 별 찍기 － 12/별 찍기 － 12.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int param = sc.nextInt();
+        for (int i = 0; i < param; ++i) {
+        	for (int j = 0; j < param; ++j) {
+        		if ((param - i - 1) <= j) {
+        			System.out.print("*");
+        		} else {
+        			System.out.print(" ");
+        		}
+        	}
+        	System.out.println();
+        }
+        for (int i = 0; i < param; ++i) {
+        	for (int j = 0; j < param; ++j) {
+        		if (i < j) {
+        			System.out.print("*");
+        		} else {
+        			System.out.print(" ");
+        		}
+        	}
+        	System.out.println();
+        }
+    }
+}
