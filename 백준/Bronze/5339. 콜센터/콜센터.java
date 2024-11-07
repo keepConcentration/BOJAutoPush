c004f813f5ff5f7b0ab783ee4d58d3c909b721ea
keepConcentration <phm543@gmail.com>
2024-11-07 13:22:30 +0900

    [Bronze V] Title: 콜센터, Time: 128 ms, Memory: 13892 KB -BaekjoonHub

---
+++ 백준/Bronze/5339. 콜센터/콜센터.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+        StringBuilder sb = new StringBuilder();
+        sb.append("     /~\\\n    ( oo|\n    _\\=/_\n   /  _  \\\n  //|/.\\|\\\\\n ||  \\ /  ||\n============\n|          |\n|          |\n|          |");
+        System.out.print(sb.toString());
+    }
+}
