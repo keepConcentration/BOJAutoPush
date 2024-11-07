6a9c8ea05d2d2b610ffdddfc661990e1c639dc56
keepConcentration <phm543@gmail.com>
2024-11-07 10:51:27 +0900

    [Gold V] Title: Fly me to the Alpha Centauri, Time: 132 ms, Memory: 13504 KB -BaekjoonHub

--- 백준/Gold/1011. Fly me to the Alpha Centauri/Fly me to the Alpha Centauri.java
+++ 백준/Gold/1011. Fly me to the Alpha Centauri/Fly me to the Alpha Centauri.java
@@ -1,17 +1,23 @@
 import java.util.Scanner;
-
-public class Main {
+public class Main{
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
-        int loop = sc.nextInt();
-		for (int i = 0; i < loop; i++) {
-			int param = -(sc.nextInt()) + sc.nextInt(),
-			    incr = 0,
-			    result = 1;
-			while (param > 0) {
-				param -= ++result % 2 == 0 ? ++incr : incr;
+		int rollNum = Integer.parseInt(sc.nextLine());
+		String[] inVal = new String[rollNum];
+		
+		for(int i=0; i<rollNum; i++) {
+			inVal[i] = sc.nextLine();
+			int param = Integer.parseInt(inVal[i].split(" ")[1]) - Integer.parseInt(inVal[i].split(" ")[0]);
+			int incr = 0;
+			int result = 0;
+			lbl: while(true) {
+				for(int j=0; j<=1; j++) {
+					param -= j==0 ? ++incr : incr;
+					result++;
+					if(param <= 0) break lbl;
+				}
 			}
-			System.out.println(result - 1);
+			System.out.println(result);
 		}
     }
 }
