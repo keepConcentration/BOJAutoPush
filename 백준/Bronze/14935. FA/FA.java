69065c6d2cc2feba795ea10f99f59a185082bd04
keepConcentration <phm543@gmail.com>
2024-11-07 18:58:10 +0900

    [Bronze IV] Title: FA, Time: 120 ms, Memory: 13992 KB -BaekjoonHub

---
+++ 백준/Bronze/14935. FA/FA.java
@@ -0,0 +1,9 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        System.out.println("FA");
+    }
+}
