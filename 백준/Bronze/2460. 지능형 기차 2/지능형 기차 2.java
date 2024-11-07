c35f99e4138a37eb3b617821231bf43514dc5be7
keepConcentration <phm543@gmail.com>
2024-11-07 13:00:27 +0900

    [Bronze III] Title: 지능형 기차 2, Time: 216 ms, Memory: 17648 KB -BaekjoonHub

---
+++ 백준/Bronze/2460. 지능형 기차 2/지능형 기차 2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+interface Main {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int result = 0;
+        int train = 0;
+        for (int i = 0; i < 10; ++i) {
+            train -= sc.nextInt();
+            train += sc.nextInt();
+            if (result < train) {
+                result = train;
+            }
+        }
+        System.out.println(result);
+    }
+}
