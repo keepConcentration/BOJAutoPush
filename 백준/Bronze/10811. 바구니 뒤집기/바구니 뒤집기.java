42367a4ea8345446f2cb72f9fa5e47b14fe3bdd3
keepConcentration <phm543@gmail.com>
2024-11-07 13:39:12 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 240 ms, Memory: 18700 KB -BaekjoonHub

---
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.stream.IntStream;
+
+class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String[] in = br.readLine().split(" ");
+        int N = Integer.parseInt(in[0]);
+        int M = Integer.parseInt(in[1]);
+
+        int[] arr = IntStream.range(0, N + 1)
+                .toArray();
+
+        for (int i = 0; i < M; ++i) {
+            in = br.readLine().split(" ");
+            int firstIndex = Integer.parseInt(in[0]);
+            int secondIndex = Integer.parseInt(in[1]);
+
+            for (int j = firstIndex, k = secondIndex; j < k; ++j, --k) {
+                int first = arr[j];
+                arr[j] = arr[k];
+                arr[k] = first;
+            }
+        }
+        br.close();
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= N; ++i) {
+            sb.append(arr[i]).append(" ");
+        }
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+}
