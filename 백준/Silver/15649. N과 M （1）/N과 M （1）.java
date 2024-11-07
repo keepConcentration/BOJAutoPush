d6fbdfc45914c6959a8f09558fd3f6dfb65bbf98
keepConcentration <phm543@gmail.com>
2024-11-07 19:00:55 +0900

    [Silver III] Title: N과 M (1), Time: 208 ms, Memory: 35640 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+
+public class Main {
+    static int n;
+    static int m;
+    static int[] arr;
+    static boolean[] chk;
+    static StringBuilder sb;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        sb = new StringBuilder();
+        
+        String[] param = br.readLine().split(" ");
+        n = Integer.parseInt(param[0]);
+        m = Integer.parseInt(param[1]);
+        
+        br.close();
+        
+        arr = new int[m];
+        chk = new boolean[n];
+        
+        dfs(0);
+        
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+    
+    public static void dfs(int depth) {
+        
+        if (depth == m) {
+            for (int val : arr) {
+                sb.append(String.valueOf(val));
+                sb.append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+        
+        for (int i = 0; i < n; ++i) {
+            if (!chk[i]) {
+                chk[i] = true;
+                arr[depth] = i + 1;
+                dfs(depth + 1);
+                chk[i] = false;
+            }
+        }
+    }
+}
