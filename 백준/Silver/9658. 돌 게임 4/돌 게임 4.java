85c86d3901fbdddf45ad10ccf8abc1a3be0ca0d6
keepConcentration <phm543@gmail.com>
2024-11-07 13:33:28 +0900

    [Silver II] Title: 돌 게임 4, Time: 76 ms, Memory: 11612 KB -BaekjoonHub

---
+++ 백준/Silver/9658. 돌 게임 4/돌 게임 4.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+        br.close();
+        if (N % 7 == 1 || N % 7 == 3) {
+            bw.write("CY");
+        } else {
+            bw.write("SK");
+        }
+        bw.flush();
+        bw.close();
+    }
+}
