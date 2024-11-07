681fd4b39ce93cbf7f249684e2c896334665c902
keepConcentration <phm543@gmail.com>
2024-11-07 13:25:48 +0900

    [Bronze IV] Title: Next in line, Time: 132 ms, Memory: 13960 KB -BaekjoonHub

---
+++ 백준/Bronze/6749. Next in line/Next in line.java
@@ -0,0 +1,14 @@
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+        
+        int result = b + b - a;
+        
+        System.out.print(result);
+    }
+}
