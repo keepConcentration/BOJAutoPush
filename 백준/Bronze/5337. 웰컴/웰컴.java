32aa9fee200fab0c93631ac2615653126d311581
keepConcentration <phm543@gmail.com>
2024-11-07 13:22:10 +0900

    [Bronze V] Title: 웰컴, Time: 124 ms, Memory: 14032 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+        StringBuilder sb = new StringBuilder();
+        sb.append(".  .   .\n|  | _ | _. _ ._ _  _\n|/\\|(/.|(_.(_)[ | )(/.");
+        System.out.print(sb.toString());
+    }
+}
