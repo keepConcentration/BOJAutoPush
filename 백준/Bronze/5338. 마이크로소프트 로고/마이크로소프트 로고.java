1849dc5f7468fa99975aecdf4f2ee47f813a8c40
keepConcentration <phm543@gmail.com>
2024-11-07 13:22:19 +0900

    [Bronze V] Title: 마이크로소프트 로고, Time: 124 ms, Memory: 13840 KB -BaekjoonHub

---
+++ 백준/Bronze/5338. 마이크로소프트 로고/마이크로소프트 로고.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+        StringBuilder sb = new StringBuilder();
+        sb.append("       _.-;;-._\n'-..-'|   ||   |\n'-..-'|_.-;;-._|\n'-..-'|   ||   |\n'-..-'|_.-''-._|");
+        System.out.print(sb.toString());
+    }
+}
