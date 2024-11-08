2a0a3e95a214f836041ee69352ef20a6ea01445f
keepConcentration <phm543@gmail.com>
2024-11-08 09:17:22 +0900

    [Bronze V] Title: A+B - 9, Time: 280 ms, Memory: 17552 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/15740. A＋B － 9/A＋B － 9.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.math.BigInteger;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        String[] params = br.readLine().split(" ");
+        BigInteger a = new BigInteger(params[0]);
+        BigInteger b = new BigInteger(params[1]);
+        
+        bw.write(a.add(b).toString() + "\n");
+        bw.flush();
+        
+    }
+}
