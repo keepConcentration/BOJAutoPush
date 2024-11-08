da507b93dae3cbf8074b2957526c8c46ccb6662a
keepConcentration <phm543@gmail.com>
2024-11-08 09:29:19 +0900

    [Bronze V] Title: Site Score, Time: 228 ms, Memory: 17608 KB -BaekjoonHub

---
+++ 백준/Bronze/20254. Site Score/Site Score.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println(s.nextInt() * 56 + s.nextInt() * 24 + s.nextInt() * 14 + s.nextInt() * 6);
+    }
+}
