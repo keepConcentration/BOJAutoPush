78cd5a193e21b6288c0563b2b8f9f42c29a2be84
keepConcentration <phm543@gmail.com>
2024-11-08 09:36:07 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 1, Time: 892 ms, Memory: 111512 KB -BaekjoonHub

---
+++ 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.java
@@ -0,0 +1,72 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+
+    static List<Integer>[] adjacentNodeArr;
+
+    static int N, M, R;
+
+    static int[] visited;
+
+    static int visitedCount = 1;
+
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String[] arr = br.readLine().split(" ");
+
+        N = Integer.parseInt(arr[0]);
+        M = Integer.parseInt(arr[1]);
+        R = Integer.parseInt(arr[2]);
+
+        visited = new int[N + 1];
+
+        adjacentNodeArr = new ArrayList[N + 1];
+        for (int i = 1; i <= N; ++i) {
+            adjacentNodeArr[i] = new ArrayList<>();
+        }
+
+        for (int i = 1; i <= M; ++i) {
+            arr = br.readLine().split(" ");
+            int startNode = Integer.parseInt(arr[0]);
+            int endNode = Integer.parseInt(arr[1]);
+            adjacentNodeArr[startNode].add(endNode);
+            adjacentNodeArr[endNode].add(startNode);
+        }
+
+        for (int i = 1; i <= N; ++i) {
+            Collections.sort(adjacentNodeArr[i]);
+        }
+
+        bfs(R);
+
+        for (int i = 1; i <= N; ++i) {
+            sb.append(visited[i]).append("\n");
+        }
+
+        br.close();
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+
+    private static void bfs(int n) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(n);
+        visited[n] = visitedCount++;
+        while (!queue.isEmpty()) {
+            int poll = queue.poll();
+            List<Integer> adjacentNodeList = adjacentNodeArr[poll];
+            for (int i = 0; i < adjacentNodeList.size(); ++i) {
+                int next = adjacentNodeList.get(i);
+                if (visited[next] == 0) {
+                    visited[next] = visitedCount++;
+                    queue.offer(next);
+                }
+            }
+        }
+    }
+}
