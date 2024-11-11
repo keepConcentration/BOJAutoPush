cf24fdcc662afc3f1edd6c4fd8318dfc0dffb7e7
keepConcentration <phm543@gmail.com>
2024-11-11 21:01:21 +0900

    [Silver III] Title: N과 M (8), Time: 112 ms, Memory: 18136 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.java
@@ -0,0 +1,55 @@
+import static java.lang.Integer.parseInt;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+
+public class Main {
+
+  static StringBuilder sb = new StringBuilder();
+  static int[] result;
+  static int[] arr;
+  static int N, M;
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    String[] strArr = br.readLine().split(" ");
+    N = parseInt(strArr[0]);
+    M = parseInt(strArr[1]);
+    arr = new int[N];
+    result = new int[M];
+
+    strArr = br.readLine().split(" ");
+    for (int i = 0; i < N; ++i) {
+      arr[i] = parseInt(strArr[i]);
+    }
+    Arrays.sort(arr);
+
+    backtracking(0, 0);
+
+    br.close();
+    bw.write(sb.toString());
+    bw.flush();
+    bw.close();
+
+  }
+
+  private static void backtracking(int start, int depth) {
+    if (depth == M) {
+      for (int i = 0; i < M; ++i) {
+        sb.append(result[i]).append(" ");
+      }
+      sb.append("\n");
+      return;
+    }
+
+    for (int i = start; i < arr.length; ++i) {
+      result[depth] = arr[i];
+      backtracking(i, depth + 1);
+    }
+  }
+}
