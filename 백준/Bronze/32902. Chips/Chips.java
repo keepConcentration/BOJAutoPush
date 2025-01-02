9c00b16f38e7ff95006f78be119240809fca3d80
keepConcentration <phm543@gmail.com>
2025-01-02 13:58:37 +0900

    [Bronze IV] Title: Chips, Time: 64 ms, Memory: 11620 KB -BaekjoonHub

---
+++ 백준/Bronze/32902. Chips/Chips.java
@@ -0,0 +1,26 @@
+import java.io.*;
+
+public class Main {
+
+    static final StringBuilder sb = new StringBuilder();
+    static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+        int K = read();
+        int N = read();
+        sb.append(N + 1).append(" ").append(K * N + 1).append("\n");
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    private static int read() throws IOException {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
+
+        return n;
+    }
+}
